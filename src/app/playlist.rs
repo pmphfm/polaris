60aaf64705b172167f9b03ff801c295c9f844724
Pmp Hfm <pmphfm@protonmail.com>
2022-12-26 18:52:24 -0800

    Moved manager.rs file contents to parent modules

--- src/app/playlist.rs
+++ src/app/playlist.rs
@@ -1,9 +1,472 @@
 mod error;
 mod m3u;
-mod manager;
 #[cfg(test)]
 mod test;
 
 pub use error::*;
 pub use m3u::*;
-pub use manager::*;
+
+use anyhow::Result;
+use core::clone::Clone;
+use diesel::prelude::*;
+use diesel::BelongingToDsl;
+use serde::{Deserialize, Serialize};
+use std::ffi::{OsStr, OsString};
+use std::fmt::Write;
+use std::path::{Path, PathBuf, MAIN_SEPARATOR};
+
+use crate::app::index::Song;
+use crate::app::vfs;
+use crate::db::{playlist_songs, playlists, songs, users, DB};
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub enum PlaylistType {
+	m3u,
+}
+
+impl Default for PlaylistType {
+	fn default() -> Self {
+		Self::m3u
+	}
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PlaylistExport {
+	pub name: String,
+	pub kind: Option<PlaylistType>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PlaylistImport {
+	// Playlist name to save as.
+	pub name: String,
+
+	// Type of input playlist.
+	pub kind: Option<PlaylistType>,
+
+	// If true, partial playlist is imported.
+	pub partial: Option<bool>,
+
+	// If true, tries to match imperfect matches.
+	pub fuzzy_match: Option<bool>,
+}
+
+#[derive(Clone)]
+pub struct Manager {
+	db: DB,
+	vfs_manager: vfs::Manager,
+}
+
+impl Manager {
+	pub fn new(db: DB, vfs_manager: vfs::Manager) -> Self {
+		Self { db, vfs_manager }
+	}
+
+	pub fn list_playlists(&self, owner: &str) -> Result<Vec<String>, Error> {
+		let mut connection = self.db.connect()?;
+
+		let user: User = {
+			use self::users::dsl::*;
+			users
+				.filter(name.eq(owner))
+				.select((id,))
+				.first(&mut connection)
+				.optional()
+				.map_err(anyhow::Error::new)?
+				.ok_or(Error::UserNotFound)?
+		};
+
+		{
+			use self::playlists::dsl::*;
+			let found_playlists: Vec<String> = Playlist::belonging_to(&user)
+				.select(name)
+				.load(&mut connection)
+				.map_err(anyhow::Error::new)?;
+			Ok(found_playlists)
+		}
+	}
+
+	pub fn save_playlist(
+		&self,
+		playlist_name: &str,
+		owner: &str,
+		content: &[String],
+	) -> Result<(), Error> {
+		let new_playlist: NewPlaylist;
+		let playlist: Playlist;
+		let vfs = self.vfs_manager.get_vfs()?;
+
+		{
+			let mut connection = self.db.connect()?;
+
+			// Find owner
+			let user: User = {
+				use self::users::dsl::*;
+				users
+					.filter(name.eq(owner))
+					.select((id,))
+					.first(&mut connection)
+					.optional()
+					.map_err(anyhow::Error::new)?
+					.ok_or(Error::UserNotFound)?
+			};
+
+			// Create playlist
+			new_playlist = NewPlaylist {
+				name: playlist_name.into(),
+				owner: user.id,
+			};
+
+			diesel::insert_into(playlists::table)
+				.values(&new_playlist)
+				.execute(&mut connection)
+				.map_err(anyhow::Error::new)?;
+
+			playlist = {
+				use self::playlists::dsl::*;
+				playlists
+					.select((id, owner))
+					.filter(name.eq(playlist_name).and(owner.eq(user.id)))
+					.get_result(&mut connection)
+					.map_err(anyhow::Error::new)?
+			}
+		}
+
+		let mut new_songs: Vec<NewPlaylistSong> = Vec::new();
+		new_songs.reserve(content.len());
+
+		for (i, path) in content.iter().enumerate() {
+			let virtual_path = Path::new(&path);
+			if let Some(real_path) = vfs
+				.virtual_to_real(virtual_path)
+				.ok()
+				.and_then(|p| p.to_str().map(|s| s.to_owned()))
+			{
+				new_songs.push(NewPlaylistSong {
+					playlist: playlist.id,
+					path: real_path,
+					ordering: i as i32,
+				});
+			}
+		}
+
+		{
+			let mut connection = self.db.connect()?;
+			connection
+				.transaction::<_, diesel::result::Error, _>(|connection| {
+					// Delete old content (if any)
+					let old_songs = PlaylistSong::belonging_to(&playlist);
+					diesel::delete(old_songs).execute(connection)?;
+
+					// Insert content
+					diesel::insert_into(playlist_songs::table)
+						.values(&new_songs)
+						.execute(&mut *connection)?; // TODO https://github.com/diesel-rs/diesel/issues/1822
+					Ok(())
+				})
+				.map_err(anyhow::Error::new)?;
+		}
+
+		Ok(())
+	}
+
+	pub fn read_playlist_real(&self, playlist_name: &str, owner: &str) -> Result<Vec<Song>, Error> {
+		let songs: Vec<Song>;
+		let song_paths: Vec<String>;
+
+		{
+			let mut connection = self.db.connect()?;
+
+			// Find owner
+			let user: User = {
+				use self::users::dsl::*;
+				users
+					.filter(name.eq(owner))
+					.select((id,))
+					.first(&mut connection)
+					.optional()
+					.map_err(anyhow::Error::new)?
+					.ok_or(Error::UserNotFound)?
+			};
+
+			// Find playlist
+			let playlist: Playlist = {
+				use self::playlists::dsl::*;
+				playlists
+					.select((id, owner))
+					.filter(name.eq(playlist_name).and(owner.eq(user.id)))
+					.get_result(&mut connection)
+					.optional()
+					.map_err(|_| Error::PlaylistNotFound(playlist_name.to_string()))?
+					.ok_or_else(|| Error::PlaylistNotFound(playlist_name.to_string()))?
+			};
+			let pid = playlist.id;
+
+			song_paths = {
+				use self::playlist_songs::dsl::*;
+				playlist_songs
+					.filter(playlist.eq(pid))
+					.select(path)
+					.order_by(ordering)
+					.get_results(&mut connection)
+					.map_err(anyhow::Error::new)?
+			};
+
+			songs = {
+				use self::playlist_songs::dsl::{path as playlist_path, *};
+				use self::songs::dsl::{id, path, *};
+				playlist_songs
+					.inner_join(songs.on(path.eq(playlist_path)))
+					.select((
+						id,
+						path,
+						parent,
+						track_number,
+						disc_number,
+						title,
+						artist,
+						album_artist,
+						year,
+						album,
+						artwork,
+						duration,
+						lyricist,
+						composer,
+						genre,
+						label,
+					))
+					.get_results(&mut connection)
+					.map_err(anyhow::Error::new)?
+			};
+
+			// Select songs. Not using Diesel because we need to LEFT JOIN using a custom column
+			// 	let query = diesel::sql_query(
+			// 		r#"
+			// 	SELECT s.id, s.path, s.parent, s.track_number, s.disc_number, s.title, s.artist, s.album_artist, s.year, s.album, s.artwork, s.duration, s.lyricist, s.composer, s.genre, s.label
+			// 	FROM playlist_songs ps
+			// 	JOIN songs s ON ps.path = s.path
+			// 	WHERE ps.playlist = ?
+			// 	ORDER BY ps.ordering
+			// "#,
+			// 	);
+			// 	let query = query.bind::<sql_types::Integer, _>(playlist.id);
+			// 	songs = query.get_results(&connection).map_err(anyhow::Error::new)?;
+		}
+
+		let mut map = std::collections::HashMap::new();
+		for (index, song) in songs.iter().enumerate() {
+			map.insert(&song.path, index);
+		}
+		let mut missing_songs = Vec::new();
+		for path in &song_paths {
+			missing_songs.push(match map.get(path) {
+				Some(index) => songs[*index].clone(),
+				None => Song::error_song(path),
+			});
+		}
+
+		log::error!("missing_songs {:?}", missing_songs);
+		log::error!("songs {:?}", songs);
+		log::error!("paths{:?}", song_paths);
+		Ok(missing_songs)
+	}
+
+	pub fn read_playlist(&self, playlist_name: &str, owner: &str) -> Result<Vec<Song>, Error> {
+		let vfs = self.vfs_manager.get_vfs()?;
+		let songs = self.read_playlist_real(playlist_name, owner)?;
+
+		// Map real path to virtual paths
+		let virtual_songs = songs
+			.into_iter()
+			.filter_map(|s| s.virtualize(&vfs))
+			.collect();
+
+		Ok(virtual_songs)
+	}
+
+	pub fn delete_playlist(&self, playlist_name: &str, owner: &str) -> Result<(), Error> {
+		let mut connection = self.db.connect()?;
+
+		let user: User = {
+			use self::users::dsl::*;
+			users
+				.filter(name.eq(owner))
+				.select((id,))
+				.first(&mut connection)
+				.optional()
+				.map_err(anyhow::Error::new)?
+				.ok_or(Error::UserNotFound)?
+		};
+
+		{
+			use self::playlists::dsl::*;
+			let q = Playlist::belonging_to(&user).filter(name.eq(playlist_name));
+			match diesel::delete(q)
+				.execute(&mut connection)
+				.map_err(anyhow::Error::new)?
+			{
+				0 => Err(Error::PlaylistNotFound(playlist_name.to_string())),
+				_ => Ok(()),
+			}
+		}
+	}
+
+	pub fn export_playlist(&self, username: &str, export: PlaylistExport) -> Result<String, Error> {
+		let songs = self.read_playlist_real(&export.name, username)?;
+		create_m3u_playlist(&songs)
+	}
+}
+
+#[derive(Identifiable, Queryable, Associations)]
+#[diesel(belongs_to(User, foreign_key = owner))]
+struct Playlist {
+	id: i32,
+	owner: i32,
+}
+
+#[derive(Identifiable, Queryable, Associations)]
+#[diesel(belongs_to(Playlist, foreign_key = playlist))]
+struct PlaylistSong {
+	id: i32,
+	playlist: i32,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = playlists)]
+struct NewPlaylist {
+	name: String,
+	owner: i32,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = playlist_songs)]
+struct NewPlaylistSong {
+	playlist: i32,
+	path: String,
+	ordering: i32,
+}
+
+#[derive(Identifiable, Queryable)]
+struct User {
+	id: i32,
+}
+
+fn get_common_path(songs: &[Song]) -> Option<OsString> {
+	if songs.len() < 2 {
+		return None;
+	}
+	let mut common_path = PathBuf::from(&songs.get(0).unwrap().path);
+	for song in &songs[1..] {
+		let next_path = Path::new(&song.path);
+		let iter = common_path.iter().zip(next_path.iter());
+		let mut temp = PathBuf::new();
+		for (c, n) in iter {
+			if c == n {
+				temp.push(c);
+			} else {
+				break;
+			}
+		}
+		common_path = temp;
+		if common_path.as_os_str().is_empty() {
+			return None;
+		}
+	}
+	let mut path = common_path.into_os_string();
+	path.push(OsStr::new(&MAIN_SEPARATOR.to_string()));
+	Some(path)
+}
+
+// Returns (common_path, buffer with with list of files).
+pub(crate) fn strip_base_path(songs: &[Song]) -> (String, String) {
+	let base_path = get_common_path(songs)
+		.unwrap_or_else(|| OsString::from(""))
+		.to_string_lossy()
+		.to_string();
+	let mut buffer = String::new();
+
+	for song in songs {
+		writeln!(
+			&mut buffer,
+			"{}",
+			song.path.strip_prefix(&base_path).unwrap()
+		)
+		.unwrap();
+	}
+	(base_path, buffer)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_no_songs() {
+		assert_eq!(strip_base_path(&[]), ("".to_string(), "".to_string()));
+	}
+
+	#[test]
+	fn test_single_song() {
+		assert_eq!(
+			strip_base_path(&[Song::test_only_from_path("abc/def")]),
+			("".to_string(), "abc/def\n".to_string())
+		);
+	}
+
+	#[test]
+	fn test_unique_paths() {
+		assert_eq!(
+			strip_base_path(&[
+				Song::test_only_from_path("abc/def"),
+				Song::test_only_from_path("def/ghi")
+			]),
+			("".to_string(), "abc/def\ndef/ghi\n".to_string())
+		);
+	}
+
+	#[test]
+	fn test_unique_paths_common_files() {
+		assert_eq!(
+			strip_base_path(&[
+				Song::test_only_from_path("abc/def"),
+				Song::test_only_from_path("def/def")
+			]),
+			("".to_string(), "abc/def\ndef/def\n".to_string())
+		);
+	}
+
+	#[test]
+	fn test_few_chars_common() {
+		assert_eq!(
+			strip_base_path(&[
+				Song::test_only_from_path("abc/def"),
+				Song::test_only_from_path("abf/ghi")
+			]),
+			("".to_string(), "abc/def\nabf/ghi\n".to_string())
+		);
+	}
+
+	#[test]
+	fn test_single_directory_common() {
+		assert_eq!(
+			strip_base_path(&[
+				Song::test_only_from_path("abc/def"),
+				Song::test_only_from_path("abc/ghi")
+			]),
+			("abc/".to_string(), "def\nghi\n".to_string())
+		);
+	}
+
+	#[test]
+	fn test_few_directories_common() {
+		assert_eq!(
+			strip_base_path(&[
+				Song::test_only_from_path("a/bc/d/ef"),
+				Song::test_only_from_path("a/bc/g/hi"),
+				Song::test_only_from_path("a/bc/j/kl")
+			]),
+			("a/bc/".to_string(), "d/ef\ng/hi\nj/kl\n".to_string())
+		);
+	}
+}
--- src/app/settings.rs
+++ src/app/settings.rs
@@ -2,11 +2,17 @@
 
 use serde::Deserialize;
 
+use diesel::prelude::*;
+use regex::Regex;
+use std::convert::TryInto;
+use std::time::Duration;
+
+use crate::app::rj::{AdminSettings, UserSettings};
+use crate::db::{misc_settings, DB};
+
 mod error;
-mod manager;
 
 pub use error::*;
-pub use manager::*;
 
 #[derive(Clone, Default)]
 pub struct AuthSecret {
@@ -23,4 +29,178 @@ pub struct Settings {
 pub struct NewSettings {
 	pub reindex_every_n_seconds: Option<i32>,
 	pub album_art_pattern: Option<String>,
+}
+
+#[derive(Clone)]
+pub struct Manager {
+	pub db: DB,
+}
+
+impl Manager {
+	pub fn new(db: DB) -> Self {
+		Self { db }
+	}
+
+	pub fn get_auth_secret(&self) -> Result<AuthSecret, Error> {
+		use self::misc_settings::dsl::*;
+		let mut connection = self.db.connect()?;
+		let secret: Vec<u8> = misc_settings
+			.select(auth_secret)
+			.get_result(&mut connection)
+			.map_err(|e| match e {
+				diesel::result::Error::NotFound => Error::AuthSecretNotFound,
+				_ => Error::Unspecified,
+			})?;
+		secret
+			.try_into()
+			.map_err(|_| Error::InvalidAuthSecret)
+			.map(|key| AuthSecret { key })
+	}
+
+	pub fn get_index_sleep_duration(&self) -> Result<Duration, Error> {
+		use self::misc_settings::dsl::*;
+		let mut connection = self.db.connect()?;
+		misc_settings
+			.select(index_sleep_duration_seconds)
+			.get_result(&mut connection)
+			.map_err(|e| match e {
+				diesel::result::Error::NotFound => Error::IndexSleepDurationNotFound,
+				_ => Error::Unspecified,
+			})
+			.map(|s: i32| Duration::from_secs(s as u64))
+	}
+
+	pub fn get_index_album_art_pattern(&self) -> Result<Regex, Error> {
+		use self::misc_settings::dsl::*;
+		let mut connection = self.db.connect()?;
+		misc_settings
+			.select(index_album_art_pattern)
+			.get_result(&mut connection)
+			.map_err(|e| match e {
+				diesel::result::Error::NotFound => Error::IndexAlbumArtPatternNotFound,
+				_ => Error::Unspecified,
+			})
+			.and_then(|s: String| {
+				Regex::new(&format!("(?i){}", &s)).map_err(|_| Error::IndexAlbumArtPatternInvalid)
+			})
+	}
+
+	pub fn read(&self) -> Result<Settings, Error> {
+		use self::misc_settings::dsl::*;
+		let mut connection = self.db.connect()?;
+
+		let settings: Settings = misc_settings
+			.select((index_sleep_duration_seconds, index_album_art_pattern))
+			.get_result(&mut connection)
+			.map_err(|_| Error::Unspecified)?;
+
+		Ok(settings)
+	}
+
+	pub fn amend(&self, new_settings: &NewSettings) -> Result<(), Error> {
+		let mut connection = self.db.connect()?;
+
+		if let Some(sleep_duration) = new_settings.reindex_every_n_seconds {
+			diesel::update(misc_settings::table)
+				.set(misc_settings::index_sleep_duration_seconds.eq(sleep_duration as i32))
+				.execute(&mut connection)
+				.map_err(|_| Error::Unspecified)?;
+		}
+
+		if let Some(ref album_art_pattern) = new_settings.album_art_pattern {
+			diesel::update(misc_settings::table)
+				.set(misc_settings::index_album_art_pattern.eq(album_art_pattern))
+				.execute(&mut connection)
+				.map_err(|_| Error::Unspecified)?;
+		}
+
+		Ok(())
+	}
+
+	pub fn get_rj_user_settings(&self) -> Result<UserSettings, Error> {
+		use crate::db::rj_user_settings::dsl::*;
+		let mut connection = self.db.connect()?;
+		let (user_scripts, enable, person_names): (Option<String>, Option<i32>, String) =
+			rj_user_settings
+				.select((scripts, enable_by_default, tts_people))
+				.get_result(&mut connection)
+				.map_err(|e| match e {
+					diesel::result::Error::NotFound => Error::IndexSleepDurationNotFound,
+					_ => Error::Unspecified,
+				})?;
+
+		Ok(UserSettings {
+			scripts: user_scripts,
+			enable_by_default: enable.map(|f| f != 0),
+			tts_people: serde_json::from_str(&person_names).unwrap(),
+		})
+	}
+
+	pub fn get_rj_admin_settings(&self) -> Result<AdminSettings, Error> {
+		use crate::db::rj_admin_settings::dsl::*;
+		let mut connection = self.db.connect()?;
+		let (url, key, enable_ssml): (Option<String>, Option<String>, i32) = rj_admin_settings
+			.select((tts_service_url, tts_text_param_key, tts_enable_ssml))
+			.get_result::<(Option<String>, Option<String>, i32)>(&mut connection)
+			.map_err(|e| match e {
+				diesel::result::Error::NotFound => Error::IndexSleepDurationNotFound,
+				_ => Error::Unspecified,
+			})?;
+		Ok(AdminSettings {
+			tts_url: url,
+			tts_key: key,
+			enable_ssml: enable_ssml != 0,
+		})
+	}
+
+	pub fn put_rj_user_settings(&self, new_settings: &UserSettings) -> Result<(), Error> {
+		use crate::db::rj_user_settings;
+		let mut connection = self.db.connect()?;
+
+		if let Some(user_script) = &new_settings.scripts {
+			diesel::update(rj_user_settings::table)
+				.set(rj_user_settings::scripts.eq(user_script))
+				.execute(&mut connection)
+				.map_err(|_| Error::Unspecified)?;
+		}
+
+		if let Some(enable) = new_settings.enable_by_default {
+			diesel::update(rj_user_settings::table)
+				.set(rj_user_settings::enable_by_default.eq(enable as i32))
+				.execute(&mut connection)
+				.map_err(|_| Error::Unspecified)?;
+		}
+
+		let person_names = serde_json::to_string(&new_settings.tts_people).unwrap();
+		diesel::update(rj_user_settings::table)
+			.set(rj_user_settings::tts_people.eq(person_names))
+			.execute(&mut connection)
+			.map_err(|_| Error::Unspecified)?;
+		Ok(())
+	}
+
+	pub fn put_rj_admin_settings(&self, new_settings: &AdminSettings) -> Result<(), Error> {
+		use crate::db::rj_admin_settings;
+		let mut connection = self.db.connect()?;
+
+		if let Some(url) = &new_settings.tts_url {
+			diesel::update(rj_admin_settings::table)
+				.set(rj_admin_settings::tts_service_url.eq(url))
+				.execute(&mut connection)
+				.map_err(|_| Error::Unspecified)?;
+		}
+
+		if let Some(key) = &new_settings.tts_key {
+			diesel::update(rj_admin_settings::table)
+				.set(rj_admin_settings::tts_text_param_key.eq(key))
+				.execute(&mut connection)
+				.map_err(|_| Error::Unspecified)?;
+		}
+
+		diesel::update(rj_admin_settings::table)
+			.set(rj_admin_settings::tts_enable_ssml.eq(new_settings.enable_ssml as i32))
+			.execute(&mut connection)
+			.map_err(|_| Error::Unspecified)?;
+		Ok(())
+	}
 }
